0a0053a976e077ad9f889b7644a8029f2be60e15
AbdulrahmanAshraf98 <abdulrahmanashraf9898@gmail.com>
2022-09-12 16:00:35 +0200

    ChannelDetails

--- src/Components/Videos/VideosCard/VideoCard.jsx
+++ src/Components/Videos/VideosCard/VideoCard.jsx
@@ -19,7 +19,7 @@ const VideoCard = ({
 				<CardMedia
 					image={snippet.thumbnails.high.url}
 					alt={snippet.title}
-					sx={{ width: { xs: "100%", sm: "380px" }, height: 180 }}
+					sx={{ width: "100%", height: 180 }}
 				/>
 				<CardContent sx={{ backgroundColor: "#1e1e1e", height: "106px" }}>
 					<Link to={`/video/${videoId}`}>
@@ -27,7 +27,7 @@ const VideoCard = ({
 							{snippet.title.slice(0, 60)}
 						</Typography>
 					</Link>
-					<Link to={`/video/${snippet.channelId}`}>
+					<Link to={`/channel/${snippet.channelId}`}>
 						<Typography variant="subtitle1" color="gray" fontWeight="bold">
 							{snippet.channelTitle.slice(0, 60)}
 							<CheckCircle
--- src/Routes/ChannelDetail/ChannelDetail.jsx
+++ src/Routes/ChannelDetail/ChannelDetail.jsx
@@ -1,7 +1,45 @@
-import React from "react";
-
+import React, { useState, useEffect, useCallback } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+import Videos from "../../Components/Videos/Videos";
+import ChannelCard from "../../Components/Videos/ChannelCard/ChannelCard";
+import { fetchDataFromApi } from "../../Utils/fetchFromApi";
 const ChannelDetail = () => {
-	return <div>ChannelDetail</div>;
+	const { id } = useParams();
+	const [channelDetail, setChannelDetail] = useState(null);
+	const [channelVideos, setChannelVideos] = useState([]);
+	const getChannel = useCallback(async () => {
+		const channelData = await fetchDataFromApi(
+			`channels?part=snippet&id=${id}`,
+		);
+		const VideosData = await fetchDataFromApi(
+			`search?channelId=${id}&part=snippet&order=date`,
+		);
+		setChannelDetail(channelData.items[0]);
+		setChannelVideos(VideosData.items);
+	}, []);
+	useEffect(() => {
+		getChannel();
+	}, [id]);
+
+	return (
+		<Box minHeight="95vh">
+			<Box>
+				<div
+					style={{ backgroundColor: "red", zIndex: 10, height: "300px" }}></div>
+				{channelDetail && (
+					<ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+				)}
+			</Box>
+			<Box display="flex" p="2">
+				<Videos
+					videos={channelVideos}
+					justifyContent={{ xs: "center" }}
+					videoItemWidth={{ xs: "100%", sm: "45%", md: "30%", lg: "320px" }}
+				/>
+			</Box>
+		</Box>
+	);
 };
 
 export default ChannelDetail;
