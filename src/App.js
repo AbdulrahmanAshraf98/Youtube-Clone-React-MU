242b7e08798a5de83b99c2107336263f4f2359ab
AbdulrahmanAshraf98 <abdulrahmanashraf9898@gmail.com>
2022-09-14 00:23:42 +0200

    addLoadingPageComponent and adding LazyLoadingRoutes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import { Box, Container } from "@mui/material";
+import React from "react";
+import { Box } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
-import Navbar from "./Components/Navbar/Navbar";
-import ChannelDetail from "./Routes/ChannelDetail/ChannelDetail";
-import Feed from "./Routes/Feed/Feed";
-import SearchFeed from "./Routes/SearchFeed/SearchFeed";
-import VideoDetail from "./Routes/VideoDetail/VideoDetail";
-
+import LoadingPage from "./Components/LoadingPage/LoadingPage";
+const Navbar = React.lazy(() => import("./Components/Navbar/Navbar"));
+const Feed = React.lazy(() => import("./Routes/Feed/Feed"));
+const SearchFeed = React.lazy(() => import("./Routes/SearchFeed/SearchFeed"));
+const VideoDetail = React.lazy(() =>
+	import("./Routes/VideoDetail/VideoDetail"),
+);
+const ChannelDetail = React.lazy(() =>
+	import("./Routes/ChannelDetail/ChannelDetail"),
+);
 function App() {
 	return (
 		<Box sx={{ backgroundColor: "#000", height: "100%", color: "white" }}>
-			<Navbar />
-			<Routes>
-				<Route index path="/" element={<Feed />} />
-				<Route path="/video/:id" element={<VideoDetail />} />
-				<Route path="/channel/:id" element={<ChannelDetail />} />
-				<Route path="/search/:searchTerm" element={<SearchFeed />} />
-			</Routes>
+			<React.Suspense fallback={<LoadingPage />}>
+				<Navbar />
+				<Routes>
+					<Route index path="/" element={<Feed />} />
+					<Route path="/video/:id" element={<VideoDetail />} />
+					<Route path="/channel/:id" element={<ChannelDetail />} />
+					<Route path="/search/:searchTerm" element={<SearchFeed />} />
+				</Routes>
+			</React.Suspense>
 		</Box>
 	);
 }
